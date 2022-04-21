ff5a0b0d24676cbdf20e74cd9512fbe1a7f6a330
StevenLi007 <sil045@ucsd.edu>
2022-04-20 19:30:25 -0700

    Completed Memoized Fib

--- Fib.java
+++ Fib.java
@@ -1,3 +1,5 @@
+import java.util.HashMap;
+
 public class Fib {
     public int recFib(int n) {
         if (n <= 2) {
@@ -7,8 +9,29 @@ public class Fib {
         }
     }
 
+    // dynamic programming refers to overlapping subproblems
+    public int memFib(int n, HashMap<Integer, Integer> memo) {
+        // if value already found, return it
+        if (memo.containsKey(n)) {
+            return memo.get(n);
+        }
+        // original base case
+        if (n <= 2) {
+            return 1;
+        }
+        // else, store the results in the memo
+        memo.put(n, memFib(n - 1 , memo) + memFib(n - 2, memo));
+        return memo.get(n);
+    }
+
     public static void main(String[] args) {
+        System.out.println(Integer.MAX_VALUE);
+
         Fib obj = new Fib();
-        System.out.println(obj.recFib(Integer.valueOf(args[0])));
+        // System.out.println(obj.recFib(6);
+
+        HashMap<Integer, Integer> memo = new HashMap<>();
+        System.out.println(obj.memFib(6, memo));
+        System.out.println(obj.memFib(30, memo));
     }
 }
