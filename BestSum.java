375800989ba9f480b040e561b5e4aaa3177e5eac
StevenLi007 <sil045@ucsd.edu>
2022-04-23 20:48:31 -0700

    memBestSum not yet functional

--- BestSum.java
+++ BestSum.java
@@ -2,46 +2,56 @@ import java.util.ArrayList;
 import java.util.HashMap;
 
 public class BestSum {
-    public boolean recBestSum(int targetSum, ArrayList<Integer> numbers) {
-        // if (targetSum == 0) {
-        //     return true;
-        // }
-        // if (targetSum < 0) {
-        //     return false;
-        // }
-        // for (Integer num : numbers) {
-        //     if (recCanSum(targetSum - num, numbers)) {
-        //         return true;
-        //     }
-        // }
-        return false;
+    public ArrayList<Integer> recBestSum(int targetSum, ArrayList<Integer> numbers) {
+        if (targetSum == 0) {
+            return new ArrayList<Integer>();
+        }
+        if (targetSum < 0) {
+            return null;
+        }
+        ArrayList<Integer> shortestCombo = null;
+        for (Integer num : numbers) {
+            ArrayList<Integer> result = recBestSum(targetSum - num, numbers);
+            if (result != null) {
+                result.add(num);
+                if (shortestCombo == null || result.size() < shortestCombo.size()) {
+                    shortestCombo = result;
+                }
+            }
+        }
+        return shortestCombo;
     }
 
-    public boolean memBestSum(int targetSum, ArrayList<Integer> numbers, HashMap<Integer, Boolean> memo) {
-        // if (targetSum == 0) {
-        //     return true;
-        // }
-        // if (targetSum < 0) {
-        //     return false;
-        // }
-        // if (memo.containsKey(targetSum)) {
-        //     return memo.get(targetSum);
-        // }
-        // for (Integer num : numbers) {
-        //     if (memCanSum(targetSum - num, numbers, memo)) {
-        //         memo.put(targetSum, true);
-        //         return true;
-        //     }
-        // }
-        // memo.put(targetSum, false);
-        return false;
+    public ArrayList<Integer> memBestSum(int targetSum, ArrayList<Integer> numbers, HashMap<Integer, ArrayList<Integer>> memo) {
+        if (targetSum == 0) {
+            return new ArrayList<Integer>();
+        }
+        if (targetSum < 0) {
+            return null;
+        }
+        if (memo.containsKey(targetSum)) {
+            return memo.get(targetSum);
+        }
+        ArrayList<Integer> shortestCombo = null;
+        for (Integer num : numbers) {
+            ArrayList<Integer> result = memBestSum(targetSum - num, numbers, memo);
+            if (result != null) {
+                result.add(num);
+                if (shortestCombo == null || result.size() < shortestCombo.size()) {
+                    shortestCombo = result;
+                }
+            }
+        }
+        memo.put(targetSum, shortestCombo);
+        return shortestCombo;
     }
     public static void main(String[] args) {
         BestSum obj = new BestSum();
         ArrayList<Integer> numbers = new ArrayList<>();
-        HashMap<Integer, Boolean> memo = new HashMap<>();
-        numbers.add(7);
-        numbers.add(14);
-        // System.out.println(obj.memCanSum(300, numbers, memo));
+        HashMap<Integer, ArrayList<Integer>> memo = new HashMap<>();
+        numbers.add(1);
+        numbers.add(2);
+        numbers.add(25);
+        System.out.println(obj.memBestSum(100, numbers, memo));
     }
 }
