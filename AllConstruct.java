fe74c55ea755541d35af3e0d753df4aaaab5792c
StevenLi007 <sil045@ucsd.edu>
2022-05-04 08:37:48 -0700

    AllConstruct translation progress

--- AllConstruct.java
+++ AllConstruct.java
@@ -1,8 +1,26 @@
 import java.util.ArrayList;
+import java.util.Arrays;
 
 public class AllConstruct {
     public ArrayList<ArrayList<String>> recAllConstruct(String target, ArrayList<String> words) {
-        return null;
+        // empty str = valid combo
+        if (target.equals("")) {
+            return new ArrayList<ArrayList<String>>();
+        }
+        // return var storing all combos
+        ArrayList<ArrayList<String>> result = new ArrayList<>();
+        // try out diff paths in words
+        for (String word : words) {
+            if (target.indexOf(word) == 0) {
+                String suffix = target.substring(word.length());
+                ArrayList<ArrayList<String>> suffixWays = recAllConstruct(suffix, words);
+                for (ArrayList<String> arr : suffixWays) {
+                    arr.add(word);
+                    result.add(arr);
+                }
+            }
+        }
+        return result;
     }
 
     public ArrayList<ArrayList<String>> memAllConstruct(String target, ArrayList<String> words) {
@@ -11,5 +29,7 @@ public class AllConstruct {
 
     public static void main(String[] args) {
         AllConstruct obj = new AllConstruct();
+        ArrayList<String> words = new ArrayList<>(Arrays.asList("purp", "p", "ur", "le", "purpl"));
+        System.out.println(obj.recAllConstruct("purple", words));
     }
 }
