cdc5aae15e4a34f19c82760b02ed86121be863f0
StevenLi007 <sil045@ucsd.edu>
2022-05-03 18:18:48 -0700

    incomplete countConstr tab

--- CountConstruct.java
+++ CountConstruct.java
@@ -41,10 +41,35 @@ public class CountConstruct {
         return total;
     }
 
+    public int tabCountConstruct(String target, ArrayList<String> words) {
+        // create table and init w/ 0
+        ArrayList<Integer> cnts = new ArrayList<>();
+        for (int i = 0; i <= target.length(); i++) {
+            cnts.add(0);
+        }
+        // 0th elem = 1 b/c empty string, trivial sol'n
+        cnts.set(0, 1);
+        // iter thru the table and check strings when elem is nonzero
+        // each time a spot is reached, its val++
+        for (int i = 0; i < cnts.size(); i++) {
+            if (cnts.get(i) != 0) {
+                for (String word : words) {
+                    if (target.substring(i).indexOf(word) == 0 && i + word.length() < cnts.size()) {
+                        cnts.set(i + word.length(), cnts.get(i + word.length()) + 1);
+                        System.out.println(cnts);
+                    }
+                }
+            }
+        }
+        // return the val of the last elem
+        return cnts.get(target.length());
+    }
+
     public static void main(String[] args) {
         CountConstruct obj = new CountConstruct();
         ArrayList<String> words = new ArrayList<>(Arrays.asList("purp", "p", "ur", "le", "purpl"));
         HashMap<String, Integer> memo = new HashMap<>();
         System.out.println(obj.memCountConstruct("purple", words, memo));
+        System.out.println(obj.tabCountConstruct("purple", words));
     }
 }
